05a4d4f81caaf04e9f0b27337f170ce148f289db
dphresh77 <dphresh657@gmail.com>
2023-07-01 16:28:11 -0500

    Config Repo

--- src/main/java/com/catalog/configrepo/ConfigRepoApplication.java
+++ src/main/java/com/catalog/configrepo/ConfigRepoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ConfigRepoApplication {
-
+	//
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigRepoApplication.class, args);
 	}
